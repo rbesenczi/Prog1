e57f3bef5e84ccb87e2add28ce2d3c7dbb501cdc
Renátó Besenczi <renato.besenczi@gmail.com>
2021-05-11 09:54:23 +0200

    Tételhúzó progi fel

---
+++ exam/tetelhuzo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <algorithm>
+
+int main()
+	// Mondj egy számot egy és nyolc között!
+{
+	std::srand(unsigned(std::time(0))); // Random inicializáció
+
+	std::vector<std::string> tetelek = // Tételek
+		{ 	
+			"Grafikus program felvezetés (PPP Ch 12)",
+			"Grafikus osztályok (PPP Ch 13)",
+			"Grafikus osztályok tervezési szempontjai (PPP Ch 14)",
+			"Grafikus osztályok: függvények és adatok kirajzolása (PPP Ch 15)", 
+			"GUI (PPP Ch 16)",
+			"Template osztályok, template függvények. (PPP Ch 19)",
+			"Generikus tárolók (PPP Ch 20)", 
+			"Generikus algoritmusok (PPP Ch 21)"
+		};
+
+	std::random_shuffle(tetelek.begin(), tetelek.end()); // Random keverés
+
+	for (int i = 0; i < tetelek.size(); ++i)
+		std::cout << i+1 << ". " << tetelek.at(i) << std::endl;
+
+	return 0;
+}
