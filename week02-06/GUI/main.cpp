15ce971e5d37531ae3cf1a1ddf3e266201f6fe0d
Renátó Besenczi <renato.besenczi@gmail.com>
2022-02-15 08:25:38 +0100

    Updated compile comment

--- week02-06/GUI/main.cpp
+++ week02-06/GUI/main.cpp
@@ -1,5 +1,5 @@
 /*
-    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
+    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images` -std=c++11
 */
 #include "Simple_window.h"
 #include "Graph.h"
